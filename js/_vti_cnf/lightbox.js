eae36fcd25d44b698e7b481d580298d2c9a0e37b
australopithecus7 <johnlyes.aus@gmail.com>
2024-06-06 11:49:12 +1000

    Add files via upload

---
+++ js/_vti_cnf/lightbox.js
@@ -0,0 +1,9 @@
+vti_encoding:SR|utf8-nl
+vti_timelastmodified:TR|12 Jul 2012 04:40:54 -0000
+vti_extenderversion:SR|12.0.0.0
+vti_backlinkinfo:VX|
+vti_author:SR|HORUS\\John
+vti_modifiedby:SR|HORUS\\John
+vti_timecreated:TR|12 Jul 2012 04:40:54 -0000
+vti_cacheddtm:TX|12 Jul 2012 04:40:54 -0000
+vti_filesize:IR|20067
---
+++ js/_vti_cnf/slider.js
@@ -0,0 +1,9 @@
+vti_encoding:SR|utf8-nl
+vti_timelastmodified:TR|12 Jul 2012 03:46:09 -0000
+vti_extenderversion:SR|12.0.0.0
+vti_author:SR|HORUS\\John
+vti_modifiedby:SR|HORUS\\John
+vti_timecreated:TR|12 Jul 2012 03:46:09 -0000
+vti_backlinkinfo:VX|
+vti_cacheddtm:TX|12 Jul 2012 03:46:09 -0000
+vti_filesize:IR|4052
